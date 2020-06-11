8e431d25ef6b065d2638efd5b0fbdfc0ea59d862
katiebayes <katie@honeycomb.io>
2020-06-11 16:36:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const tableHeaders = [
+  'Name',
+  'Birthday',
+  'Fav color'
+]
+
+const tableValuesUsingJSON = [
+  {
+    name: 'Katie',
+    birthday: 'June 9',
+    favColor: 'Purple'
+  },
+  {
+    name: 'Owen',
+    birthday: 'Jan 6',
+    favColor: 'Green'
+  }
+]
+
+const Th = props => {
+  return (
+    <th scope="col">
+      {props.children}
+    </th>
+  )
+}
+
+const Tr = props => {
+  const { values } = props;
+  const keys = Object.keys(values);
+  console.log(keys);
+  return (
+    <tr>
+      {keys.map(eachKey => {
+        return (
+          <td>{values[eachKey]}</td>
+        )
+      })}
+    </tr>
+  )
+}
+
+const Table = props => {
+  const numberOfColumns = tableHeaders.length;
+  return (
+    <table>
+      <colgroup>
+        <col />
+        <col span={numberOfColumns} />
+        <col span={numberOfColumns} />
+      </colgroup>
+      <thead>
+      <tr>
+        {props.headers.map(each => {
+          return (
+            <Th>{each}</Th>
+          )
+        })}
+      </tr>
+      </thead>
+      <tbody>
+        {props.children}
+      </tbody>
+    </table>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="App">
+      <h1>Accessibility Tips</h1>
+      <section>
+        <h2>
+        </h2>
+        <pre style={{textAlign: "left"}}>
+          {`
+          h2 {
+            font-size: 1.2em;
+            color: purple;
+          }
+          `}
+        </pre>
+      </section>
+      <section>
+        <h2>Tables</h2>
+        <Table headers={tableHeaders}>
+          {tableValuesUsingJSON.map(each => {
+            return <Tr values={each} />
+          })}
+        </Table>
+      </section>
+    </article>
   );
 }
 
